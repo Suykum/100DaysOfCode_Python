640b8f5f7c0fd0122e11e734770090663b5b75c6
Suykum <suikum2006@gmail.com>
2020-12-16 12:54:51 +0300

    OOP_Coffee_Machine

---
+++ coffe_maker/main.py
@@ -0,0 +1,22 @@
+from coffe_maker.menu import Menu
+from coffe_maker.coffee_maker import CoffeeMaker
+from coffe_maker.money_machine import MoneyMachine
+
+is_on = True
+menu = Menu()
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
