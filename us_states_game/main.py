f6ea8378653cfeace5eb1856d6bbce723da12119
Suykum <42752036+Suykum@users.noreply.github.com>
2020-12-21 22:05:49 +0300

    us_states_game

--- us_states_game/main.py
+++ us_states_game/main.py
@@ -10,14 +10,12 @@ turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
-missing_states = []
+
 
 while len(guessed_states) <= 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_lean")
         break
