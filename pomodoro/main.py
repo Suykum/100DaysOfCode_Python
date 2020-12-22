42c3485076b9bd776b0ec8e7add10c2e875cf402
Suykum <42752036+Suykum@users.noreply.github.com>
2020-12-22 18:02:32 +0300

    pomodoro_update

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,26 +1,30 @@
 import math
 from tkinter import *
-import time
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
+
 # ---------------------------- TIMER RESET ------------------------------- #
+
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
-    title_lable.config(text="Timer", fg=GREEN)
+    title_label.config(text="Timer", fg=GREEN)
     check_label.config(text="")
 
+
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+
 def start_timer():
     global reps
     reps += 1
@@ -29,15 +33,16 @@ def start_timer():
     long_break_sec = LONG_BREAK_MIN * 60
     if reps % 8 == 0:
         count_down(long_break_sec)
-        title_lable.config(text="Break", fg=RED)
+        title_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count_down(short_break_sec)
-        title_lable.config(text="Break", fg=PINK)
+        title_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
-        title_lable.config(text="Work", fg=GREEN)
+        title_label.config(text="Work", fg=GREEN)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -51,18 +56,19 @@ def count_down(count):
     else:
         start_timer()
         mark = ""
-        work_session = math.floor(reps/2)
+        work_session = math.floor(reps / 2)
         for _ in range(work_session):
             mark += "✓"
         check_label.config(text=mark)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
-title_lable = Label(text="Timer", font=(FONT_NAME, 35), fg=GREEN, bg=YELLOW)
-title_lable.grid(column=1, row=0)
+title_label = Label(text="Timer", font=(FONT_NAME, 35), fg=GREEN, bg=YELLOW)
+title_label.grid(column=1, row=0)
 
 start_button = Button(text="Start", bg=YELLOW, command=start_timer)
 start_button.grid(column=0, row=2)
@@ -74,11 +80,9 @@ check_label = Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 25, "bold"))
 check_label.grid(column=1, row=3)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img=PhotoImage(file="tomato.png")
+tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
-
-
 
 window.mainloop()
