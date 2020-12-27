3860869b0e6a742d4cac78ccccbe07365f7677f7
Suykum <42752036+Suykum@users.noreply.github.com>
2020-12-27 18:54:08 +0300

    BeautifulSoup

--- bsoup/movie_scraping.py
+++ bsoup/movie_scraping.py
@@ -5,11 +5,9 @@ response = requests.get("https://www.empireonline.com/movies/features/best-movie
 movies_page = response.text
 soup = BeautifulSoup(movies_page, "html.parser")
 title_list = soup.find_all(name="h3", class_="title")
-movie_list = [movie.getText().split(")") for movie in title_list]
-for movie in movie_list:
-    if len(movie) < 2:
-        movie_list.remove(movie)
-movie_list_final = [movie[1] for movie in movie_list]
+movie_list = [movie.getText() for movie in title_list]
+movie_list_final = movie_list[::-1]
+
 with open("must_watch_movies.txt", "a", encoding="utf8") as file:
-    for n in range(0, len(movie_list_final) - 1):
-        file.write(f"{n + 1}{movie_list_final[n]}\n")
+    for movie in movie_list_final:
+        file.write(f"{movie}\n")
